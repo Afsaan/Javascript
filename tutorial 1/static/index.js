ad6dcd3560efb892cabf756920ce0a918f8c3c11
afsan <khanafsaan11@gmail.com>
2020-06-13 15:19:17 +0530

    javascript tutorial

---
+++ tutorial 1/static/index.js
@@ -0,0 +1,10 @@
+
+console.time('your code took')
+console.log('hello console');
+console.warn('this is a warning message');
+console.log([1,2,3,4])
+console.log({ Name : 'Afsan' , Surname : 'Khan'})
+console.table({ Name : 'Afsan' , Surname : 'Khan'})
+console.timeEnd('your code took')
+console.assert( 124 < 96 , 'error')
+console.error('this is an error')
---
+++ tutorial 2/static/index.js
@@ -0,0 +1,20 @@
+var name = 'Afsan';
+console.log(name);
+
+var surname;
+console.log(surname);
+
+
+const age = 45
+console.log(age)
+
+// const gender; this is not possible
+
+let city = 'Goa'
+
+{
+    let city = 'kolkata';
+    console.log(city)
+}
+
+console.log(city)

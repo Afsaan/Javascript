e792d02b9d2d6721213e26632a3a60caeb686cfd
afsan <khanafsaan11@gmail.com>
2020-06-14 14:24:54 +0530

    tutorial 7

---
+++ tutorial 7/static/index.js
@@ -0,0 +1,33 @@
+//if else
+
+var val = 34
+
+
+if (val<34)
+{
+    console.log('value is less than 34')
+
+}
+else if (val == 34)
+{
+    console.log('value is equal to 34')
+
+}
+else {
+    console.log('value is more than 34')
+}
+
+
+
+//switch cases
+switch (val) {
+    case 23:
+        console.log('value is 23');
+        break;
+    case 38:
+        console.log('value is 38')
+        break;
+
+    default:
+        console.log('none of the above')
+}

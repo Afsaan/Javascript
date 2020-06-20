bce656867b0545fe4a8a7913767050851aff7e62
afsan <khanafsaan11@gmail.com>
2020-06-20 22:14:30 +0530

    tutorial 10

---
+++ tutorial 10/static/index.js
@@ -0,0 +1,18 @@
+a = document
+console.log(a)
+
+console.log(a.all) //gives all the tag that are used in the html page
+
+
+console.log(a.body) //shows everthing that is inside the body
+
+console.log(a.forms) //gives all the forms in the html page
+
+
+console.log(a.links) //to get all the links 
+
+
+console.log(a.links[0].href) //to get a particular link
+
+
+console.log(a.scripts) //to get all the script tags
